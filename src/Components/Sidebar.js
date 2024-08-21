139ca31ac89d627c49585fdc363baf831d26dd69
Sahajad <sahajad.ansari@heraldcollege.edu.np>
2024-08-21 10:47:01 +0545

    Components

---
+++ src/Components/Sidebar.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+// export const Sidebar = () =>{
+//     return(
+//         <div>
+//             Sidebar
+//         </div>
+//     )
+// }
+function Sidebar(){
+    return(
+        <div>
+            Sidebar
+        </div>
+    )
+}
+export default Sidebar;
+// export default Sidebar;
